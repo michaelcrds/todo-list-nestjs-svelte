b8e447aa2c8a6cd9d6f141e5317b1b928f24908c
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-03-10 14:57:31 -0300

    Security feature, rate limiting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard } from '@nestjs/throttler/dist/throttler.guard';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Task } from './tasks/entities/task.entity';
 import { TasksModule } from './tasks/tasks.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -28,9 +31,30 @@ import { TasksModule } from './tasks/tasks.module';
       entities: [Task],
       synchronize: true,
     }),
+    /* 
+      O thottler é uma solução do nestJs que usamos
+      para ter um controle de quantos acessos nossa API pode receber e por quem,
+      assim podemos evitar um ataque de DDoS.
+
+      Aqui estou fazendo uma configuração padrão para toda a API
+      mas poderiamos adicionar diferentes regras por modules ou end-points.
+    */
+    ThrottlerModule.forRoot({
+      // A cada um minuto
+      ttl: 60,
+      // Podemos receber até 10 requisições em cada end-point
+      limit: 10,
+    }),
     TasksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Adicionando o Thottler em toda aplicação
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
