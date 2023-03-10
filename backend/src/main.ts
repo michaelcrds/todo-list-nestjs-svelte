cc350656d6577726bfc707a69853db42d70a6cc4
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-03-10 14:52:53 -0300

    Security feature, CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Class validator
   app.useGlobalPipes(new ValidationPipe());
+
+  /* 
+    CORS (Cross-origin Resource Sharing) é um mecanismo de segurança
+    para compartilhamento de recursos entre diferentes origens.
+    
+    Aqui estou usando a configuração padrão, mas em um caso real 
+    poderiamos restrigir o acesso da API para apenas dominios que pertence a VLGI.
+  */
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
