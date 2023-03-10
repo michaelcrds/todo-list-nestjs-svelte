5d806cbe9ad4e0989fb2891bb501362883328bc5
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-03-10 14:02:57 -0300

    Adding comments to Task DTO

--- backend/src/tasks/dto/create-task.dto.ts
+++ backend/src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty } from 'class-validator';
 
+/* 
+  DTO - Data transfer object 
+  Essa classe representa o objeto que vai ser trafegado para criar uma nova Task
+*/
 export class CreateTaskDto {
+  // Validando os campos necessários para criar uma Task
   @IsNotEmpty()
   title: string;
   description: string;
