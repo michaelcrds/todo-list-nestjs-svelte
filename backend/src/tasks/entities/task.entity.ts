32cf6afa621d6e8ecc10fe214a815a58ed28584f
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-03-10 13:43:04 -0300

    Task entity

--- backend/src/tasks/entities/task.entity.ts
+++ backend/src/tasks/entities/task.entity.ts
@@ -1,1 +1,28 @@
-export class Task {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  done: boolean;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
