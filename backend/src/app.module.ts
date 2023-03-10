43f5c4e5f52bd936fb9f9fbe0ee0ab958eba822a
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-03-10 12:51:26 -0300

    Security feature, environment variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    /*
+      Para maior segurança de dados como usuário, senhas, chaves sercretas, entre outros dados sensíveis 
+      é altamente recomendavel o uso de váriaveis de ambiente.
+
+      configurando variavéis de ambiente
+    */
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
